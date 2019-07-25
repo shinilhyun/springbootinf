dbe3755538e9aba47b932380465bfce2256527a6
aiden_shin <shin88@ejbt.co.kr>
2019-07-25 23:55:40 +0900

    Initial commit

---
+++ src/main/java/com/shin/springbootinf/SpringbootinfApplication.java
@@ -0,0 +1,13 @@
+package com.shin.springbootinf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootinfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootinfApplication.class, args);
+    }
+
+}
