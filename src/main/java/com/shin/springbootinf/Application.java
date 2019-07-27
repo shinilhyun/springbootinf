f72456a4ce962a617d02f3579d94c41b6f504139
aiden_shin <shin88@ejbt.co.kr>
2019-07-27 20:29:08 +0900

    외부설정 properties test

---
+++ src/main/java/com/shin/springbootinf/Application.java
@@ -0,0 +1,17 @@
+package com.shin.springbootinf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(Application.class);
+        app.setWebApplicationType(WebApplicationType.NONE);
+        app.run(args);
+    }
+
+}
---
+++ src/main/java/com/shin/springbootinf/SampleRunner.java
@@ -0,0 +1,19 @@
+package com.shin.springbootinf;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+
+public class SampleRunner implements ApplicationRunner {
+    @Autowired
+    ShinProperties shinProperties;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        System.out.println("=========================");
+        System.out.println(shinProperties.getName());
+        System.out.println(shinProperties.getAge());
+        System.out.println(shinProperties.getFullName());
+        System.out.println("=========================");
+    }
+}
