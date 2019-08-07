651e3d3e0e3a417bf236e824210d7e59a73ef702
aiden_shin <shin88@ejbt.co.kr>
2019-08-07 18:45:00 +0900

    JPA 연동 및 테스트1

---
+++ src/test/java/com/shin/springbootinf/account/AccountRepositoryTest.java
@@ -0,0 +1,39 @@
+package com.shin.springbootinf.account;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.SQLException;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest    //슬라이스디비 (추천).. 인베디드 디비 사용하여 테스트 ex) h2
+//@SpringBootTest   //실제 디비 사용 (느림)
+public class AccountRepositoryTest {
+
+    @Autowired
+    DataSource dataSource;
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    AccountRepository accountRepository;
+
+    @Test
+    public void di() throws SQLException {
+        try (Connection connection = dataSource.getConnection()) {
+            DatabaseMetaData metaData = connection.getMetaData();
+            System.out.println(metaData.getURL());
+            System.out.println(metaData.getDriverName());
+            System.out.println(metaData.getUserName());
+        }
+    }
+}
